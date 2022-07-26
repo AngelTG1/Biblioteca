fca60063b98ad45144443b7625daf11ca924cbcc
AngelTg <angeltgz30@gmail.com>
2022-07-26 12:59:18 -0500

    proyectoRecuC2

--- src/com/upchiapas/Biblioteca/modelos/Biblioteca.java
+++ src/com/upchiapas/Biblioteca/modelos/Biblioteca.java
@@ -7,11 +7,7 @@ public class Biblioteca {
 
 
     public static void registroLibro() {
-        LIbro objetoLibro = null;
-        Scanner entrada = new Scanner(System.in);
-        boolean aux = false;
-        String categoria;
-        int opcion = 0;
+
 
         LIbro libro1 = new LIbro(3, "Algebra", "Matematicas");
         LIbro libro2 = new LIbro(2, "Lenguas", "Ingles");
@@ -24,10 +20,11 @@ public class Biblioteca {
         listaLibro[2] = libro3;
         listaLibro[3] = libro4;
         listaLibro[4] = libro5;
-
+        String libro;
+        System.out.println("-----------------------------------------");
         for(int i=0; i<5; i++) {
-            System.out.println("Id: "+listaLibro[i].getId()+" Nombre: "+listaLibro[i].getNombre());
-
+         //   System.out.println("Id: "+listaLibro[i].getId()+" Nombre: "+listaLibro[i].getNombre()+ " Categoria: "+listaLibro[i].getCategoria());
+           libro="Id: "+listaLibro[i].getId()+" Nombre: "+listaLibro[i].getNombre()+ " Categoria: "+listaLibro[i].getCategoria();
         }
     }
 
@@ -43,10 +40,18 @@ public class Biblioteca {
             }
         }
 
-        System.out.println("//////");
-        System.out.println("Biblioteca ordenada:");
+
+
         for (int i=0; i< listaLibro.length; i++){
-            System.out.println(listaLibro[i].getNombre());
+           // System.out.println(listaLibro[i].getNombre());
+        }
+    }
+
+    public static void consultaGeneral(){
+        System.out.println("-------------------------");
+        System.out.println("Biblioteca ordenada +- Mayor a Menor");
+        for(int i=(listaLibro.length-1); i>-1; i--){
+            System.out.println(" ID: "+listaLibro[i].getId()+" Titulo: "+listaLibro[i].getNombre());
         }
     }
 
@@ -65,8 +70,7 @@ public class Biblioteca {
         for(int i=0; i<listaLibro.length; i++){
             if(categoria.equals(listaLibro[i].getCategoria())){
                 System.out.println("Categoria encontrada:");
-                System.out.println("id: "+listaLibro[i].getId());
-                System.out.println("Nombre: "+listaLibro[i].getNombre());
+                System.out.println("Id: "+listaLibro[i].getId()+" Nombre: "+listaLibro[i].getNombre());
             }
         }
     }
@@ -75,22 +79,29 @@ public class Biblioteca {
         Scanner entrada = new Scanner(System.in);
         int op;
         do{
-            System.out.println("1. Visualizar Libros");
+            System.out.println("=========================");
+            System.out.println("1. Consulta general");
             System.out.println("2. buscar LIbros");
             System.out.println("3. salir");
+            System.out.println("=========================");
+            System.out.print("Selecciones una opcion: ");
             op = entrada.nextInt();
             switch (op){
                 case 1:
                     registroLibro();
                     ordenarLibro();
+                    consultaGeneral();
                     break;
                 case 2:
-                    System.out.println("ingresa id: ");
+                    registroLibro();
+                    ordenarLibro();
+                    System.out.println("ingresa Categoria: ");
                     String op1;
                     op1=entrada.next();
                     buscarCategoriaL(op1);
                     break;
                 case 3:
+
                     break;
             }
         }while(op !=3);
